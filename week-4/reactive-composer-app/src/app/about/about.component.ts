cb177304cd4dd43e62b24d1eefb922493e95ba88
Wendy Leon <pixelmamma@gmail.com>
2021-01-21 21:20:43 -0500

    Adding 4.3

---
+++ week-4/reactive-composer-app/src/app/about/about.component.ts
@@ -0,0 +1,23 @@
+/*============================================
+; Title:  Composer app
+; Author: Professor Krasso
+; Date:   10 January 2021
+; Modified by: Wendy Leon
+; Description: composer app
+;===========================================
+*/
+import { Component, OnInit } from '@angular/core';
+// about selector and paths
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ week-4/reactive-composer-app/src/app/app.component.ts
@@ -0,0 +1,23 @@
+/*============================================
+; Title:  composer list
+; Author: Professor Krasso
+; Date:   14 January 2021
+; Modified by: Wendy Leon
+  Edited:
+; Description: composer list January
+;===========================================
+*/
+
+//external files import
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// assignment name var
+export class AppComponent {
+  assignment: string = 'Exercise 4.3 - Handling Events with Observables';
+}
