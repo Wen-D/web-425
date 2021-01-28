89382649f5bdee7f5df57b604467b19b1ab3e605
Wendy Leon <pixelmamma@gmail.com>
2021-01-28 00:10:35 -0500

    Adding 5.1 files

---
+++ week-5/in-n-out-bookspt1/src/app/about/about.component.ts
@@ -0,0 +1,24 @@
+/* ============================================
+; Title:  In & Out Book Part Uno
+; Author: Professor Krasso
+; Date:   27 January 2021
+; Modified by: Wendy Leon
+; Description: In & Out Book Part Uno
+;===========================================
+*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ week-5/in-n-out-bookspt1/src/app/app.module.ts
@@ -0,0 +1,50 @@
+/* ============================================
+; Title:  In & Out Book Part Uno
+; Author: Professor Krasso
+; Date:   27 January 2021
+; Modified by: Wendy Leon
+; Description: In & Out Book Part Uno
+;===========================================
+*/
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BookListComponent } from './book-list/book-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+// material modules import
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent,
+    ContactComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    //adding material modules
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    FlexLayoutModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
