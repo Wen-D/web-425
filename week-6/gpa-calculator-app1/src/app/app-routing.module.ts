5048784e08f7f9662d9c33f4f39926c8634246fb
Wendy Leon <pixelmamma@gmail.com>
2021-02-05 23:40:37 -0500

    Adding 6.3

---
+++ week-6/gpa-calculator-app1/src/app/app-routing.module.ts
@@ -0,0 +1,49 @@
+/* ============================================
+; Title:  GPA calculator app1
+; Author: Professor Krasso
+; Date:   5 February 2021
+; Modified by: Wendy Leon
+; Description:  gpa calculator app1
+;===========================================
+*/
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+/* importing components we created */
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { HomeComponent } from './home/home.component';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  },
+  {/* 404 */
+    path: 'session',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'not-found',
+        component: NotFoundComponent
+      }
+    ]
+  },
+  { /* 404 */
+    path: '**',
+    redirectTo: 'session/not-found'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ week-6/gpa-calculator-app1/src/app/app.component.ts
@@ -0,0 +1,20 @@
+/* ============================================
+; Title:  GPA calculator app1
+; Author: Professor Krasso
+; Date:   5 February 2021
+; Modified by: Wendy Leon
+; Description:  gpa calculator app1
+;===========================================
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  /*app title */
+  title = 'gpa-calculator-app1';
+}
